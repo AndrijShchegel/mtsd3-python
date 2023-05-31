51ec45b80fb2387083469ffa78f03549b8f37954
AndrijShchegel <andrii.shchegel@gmail.com>
2023-05-31 21:07:10 +0300

    add two matrixes 10*10 and combine them

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,8 +1,17 @@
+import numpy
 from fastapi import APIRouter
 
 router = APIRouter()
 
 
-@router.get('')
-def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+@router.get('/matrix')
+def matrix() -> dict:
+    first = numpy.random.random_integers(0, 9, (10, 10))
+    second = numpy.random.random_integers(0, 9, (10, 10))
+    combine = numpy.dot(first, second)
+    return {
+        'first matrix': first.tolist(),
+        'second matrix': second.tolist(),
+        'combine matrix': combine.tolist()
+    }
+
